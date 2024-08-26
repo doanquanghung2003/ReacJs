7312d61deb25563660808b7fb5ee17db0d9804f6
Đoàn Quang Hưng <dhw972300@gmail.com>
2024-08-26 18:51:49 +0700

    update

--- TS2405_REACTJS/demoreactjs/src/App.js
+++ TS2405_REACTJS/demoreactjs/src/App.js
@@ -1,10 +1,18 @@
 import Button from './components/Button/Button';
 import './App.css'
 import NavBar from './components/Header/NavBar';
+import { Route, Router } from 'react-router-dom';
+import Home from './pages/Home';
 function App() {
   return (
     <div className="App">
         <NavBar/>
+        <main>
+          <Route>
+            <Route path='/' Component={Home}></Route>
+            <Route path='/about-us' Component={About}></Route>
+          </Route>
+        </main>
     </div>
   );
 }
